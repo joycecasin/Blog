779cd2fbdaa2fee048c3c11fb09777a4108e6df1
joyce <joyce.casin@hotmail.com>
2020-05-18 14:41:28 +0200

    Toevoegen navbar

---
+++ includes/content.php
@@ -0,0 +1,35 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-lg-4 mt-4">
+            <div class="card" style="width: 18rem;">
+                <img src="admin/img/dorp.jpg" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Titel </h5>
+                    <p class="card-text">Omschrijving</p>
+                    <a href="blog1.php" class="btn btn-primary">Lees meer</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 mt-4">
+            <div class="card" style="width: 18rem;">
+                <img src="admin/img/dorp.jpg" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Titel</h5>
+                    <p class="card-text">Omschrijving</p>
+                    <a href="#" class="btn btn-primary">Lees meer</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 mt-4">
+            <div class="card" style="width: 18rem;">
+                <img src="admin/img/dorp.jpg" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Titel</h5>
+                    <p class="card-text">Omschrijving</p>
+                    <a href="#" class="btn btn-primary">Lees meer</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
